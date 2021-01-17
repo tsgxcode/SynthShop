711da5fe27432bba82f7769b83f001fee9c91a54
TSG <TSG@TSGAC>
2021-01-17 11:15:46 -0500

    1.17.2021 Practicing Do while loops

---
+++ TheSynthShop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TheSynthShop
+{
+    class Program
+    {
+     
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Synth Shop!");
+
+            Console.WriteLine("Buy a synth? ");
+            string OneSynth = Console.ReadLine();
+
+               string Yes = "Yes";
+
+        do{
+            Console.WriteLine("i = {Yes}", Yes);
+            
+
+        }while (Yes == "Yes");
+
+        }
+
+
+    }
+}
