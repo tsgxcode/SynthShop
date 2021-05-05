83e3b7feed578acb4d57bf7ed004f6befad594a2
TSG <TSG@TSGAC>
2021-05-05 10:24:52 -0400

    5.May.2021 Removed do/while

--- TheSynthShop/Program.cs
+++ TheSynthShop/Program.cs
@@ -12,33 +12,11 @@ namespace TheSynthShop
         {
             // Shopping app. Uaser purches Modualar synth moduals
             Console.WriteLine("The Synth Shop!");
-
-            //Master loop
-            do
-            {
-                Console.WriteLine("New Synth Modual? ");
-
-                addNewModual();
-                Console.WriteLine("Would you like to see the current list? ");
-                if (Console.ReadLine().ToUpper() == "YES") 
-                {
-                    addNewModual();
-                }
-                Console.WriteLine("Buy Another? ");
-
-            } while (Console.ReadLine().ToUpper() == "YES");
-
-
-             static void addNewModual()
-            {
-
-                Console.WriteLine("What kind? ");
-                string OneSynth = Console.ReadLine();
-
-
-                File.AppendAllText(@"C:\Users\TSG\source\repos\SynthShop\TheSynthShop\data.txt", (OneSynth) + Environment.NewLine);
-            }
-
+            
+            
+            
+            
+            //File.AppendAllText(@"C:\Users\TSG\source\repos\SynthShop\TheSynthShop\data.txt", (OneSynth) + Environment.NewLine);
         }
     }
 }
